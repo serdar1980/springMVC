647aa7a6d743c0eebdd54c1807f59a8cfde156de
Скоморохов Сергей <skomorokhov@msk.i-teco.ru>
2016-12-28 10:49:30 +0300

    Add Entity scan

--- src/main/java/ru/serdar1980/customers/CustomerServiceApplication.java
+++ src/main/java/ru/serdar1980/customers/CustomerServiceApplication.java
@@ -3,11 +3,13 @@ package ru.serdar1980.customers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("ru.serdar1980")
+@EntityScan("ru.serdar1980.domain")
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
